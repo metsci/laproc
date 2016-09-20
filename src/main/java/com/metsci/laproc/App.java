a6d7e569c0fe56bad1642af08551ec40d9fa3ecb
chris <christopher.malinosky@gmail.com>
2016-09-20 18:41:10 -0400

    Display "ScatterPlotExample" in a tab

--- src/main/java/com/metsci/laproc/App.java
+++ src/main/java/com/metsci/laproc/App.java
@@ -1,4 +1,8 @@
 package com.metsci.laproc;
+import com.jogamp.opengl.util.FPSAnimator;
+import com.metsci.glimpse.canvas.NewtSwingGlimpseCanvas;
+
+import javax.swing.*;
 
 /**
  * Hello world!
@@ -8,6 +12,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        final JFrame frame = new JFrame( "Glimpse Example" );
+        frame.setSize( 800, 800 );
+        frame.setVisible( true );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+        JTabbedPane tabbedPane = new JTabbedPane();
+        frame.add(tabbedPane);
+        NewtSwingGlimpseCanvas canvas = new NewtSwingGlimpseCanvas( );
+        canvas.addLayout(new ScatterPlot().getLayout());
+        new FPSAnimator( canvas.getGLDrawable( ), 120 ).start( );
+        tabbedPane.add("ScatterPlot",canvas);
     }
 }
