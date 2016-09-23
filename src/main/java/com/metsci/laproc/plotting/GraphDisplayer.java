5ca6e9ec42ee73bc75bd2b43fc737cd0c294cb73
chris <christopher.malinosky@gmail.com>
2016-09-23 05:41:43 -0400

    Changed Variable names to be more readable and made legend change based on GraphableData in the graph

--- src/main/java/com/metsci/laproc/plotting/GraphDisplayer.java
+++ src/main/java/com/metsci/laproc/plotting/GraphDisplayer.java
@@ -11,62 +11,62 @@ import com.metsci.laproc.plotting.Graph;
 import com.metsci.laproc.plotting.GraphableData;
 
 /**
- * Creates a Glimpse plot for a graph object
+ * Creates a Glimpse plot for a Graph
  * Created by malinocr on 9/20/2016.
  */
 public class GraphDisplayer implements GlimpseLayoutProvider
 {
     Graph graph;
 
-    public GraphDisplayer(Graph graph )
+    public GraphDisplayer(Graph graph)
     {
         this.graph = graph;
     }
 
     public SimplePlot2D getLayout()
     {
-        // create a plot frame
+        // Create a plot frame
         SimplePlot2D plot = new SimplePlot2D( );
 
         Axis xAxis = graph.getXAxis();
         Axis yAxis = graph.getYAxis();
 
-        // set axis labels and chart title
+        // Set axis labels and chart title
         plot.setTitle( graph.getTitle() );
         plot.setAxisLabelX( xAxis.getName() );
         plot.setAxisLabelY( yAxis.getName() );
 
-        // set the x, y initial axis bounds
+        // Set the x, y initial axis bounds
         plot.setMinX( xAxis.getMin() );
         plot.setMaxX( xAxis.getMax() );
 
         plot.setMinY( yAxis.getMin() );
         plot.setMaxY( yAxis.getMax() );
 
-        // don't show the square selection box, only the x and y crosshairs
+        // Only show the x and y crosshairs
         plot.getCrosshairPainter( ).showSelectionBox( false );
 
-        // draws a line for each set of graphable data
-        for(GraphableData g : graph.getData()){
-            XYLinePainter series = createXYLinePainter( g );
-            plot.addPainter( series );
-        }
-
-        // add a painter to display the x and y position of the cursor
-        CursorTextPainter cursorPainter = new CursorTextPainter( );
-        plot.addPainter( cursorPainter );
-
-        // don't offset the text by the size of the selection box, since we aren't showing it
-        cursorPainter.setOffsetBySelectionSize( false );
-
+        //Set up Legend
         LineLegendPainter legend = new LineLegendPainter( LegendPlacement.SE );
         legend.setOffsetY( 10 );
         legend.setOffsetX( 100 );
-        legend.addItem( "Series 1", GlimpseColor.fromColorRgba( 1.0f, 0.0f, 0.0f, 0.8f ) );
-        legend.addItem( "Series 2", GlimpseColor.fromColorRgba( 0.0f, 0.0f, 1.0f, 0.8f ) );
         legend.setLegendItemWidth( 60 );
 
-        // add the legend painter to the top of the center GlimpseLayout
+        // Draws a line for each set of graphable data
+        for(GraphableData lineData : graph.getData()){
+            XYLinePainter linePainter = createXYLinePainter( lineData );
+            plot.addPainter( linePainter );
+            legend.addItem( lineData.getName() , GlimpseColor.fromColorRgba( 1.0f, 0.0f, 0.0f, 0.8f ) );
+        }
+
+        // Add a painter to display the x and y position of the cursor
+        CursorTextPainter cursorPainter = new CursorTextPainter( );
+        plot.addPainter( cursorPainter );
+
+        // Don't offset the text by the size of the selection box
+        cursorPainter.setOffsetBySelectionSize( false );
+
+        // Add the legend painter to the top of the center GlimpseLayout
         plot.getLayoutCenter( ).addPainter( legend );
 
         return plot;
@@ -74,12 +74,12 @@ public class GraphDisplayer implements GlimpseLayoutProvider
 
     public static XYLinePainter createXYLinePainter( GraphableData data )
     {
-        XYLinePainter series = new XYLinePainter( );
-        series.setData( data.getXValues(), data.getYValues() );
-        series.setLineColor( GlimpseColor.fromColorRgba( 1.0f, 0.0f, 0.0f, 0.8f ) );
-        series.setLineThickness( 1.5f );
-        series.showPoints( false );
+        XYLinePainter linePainter = new XYLinePainter( );
+        linePainter.setData( data.getXValues(), data.getYValues() );
+        linePainter.setLineColor( GlimpseColor.fromColorRgba( 1.0f, 0.0f, 0.0f, 0.8f ) );
+        linePainter.setLineThickness( 1.5f );
+        linePainter.showPoints( false );
 
-        return series;
+        return linePainter;
     }
 }
