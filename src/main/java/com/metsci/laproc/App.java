9755805f2ae62978f2a7f7b203f251cce0271279
patterjm <patterjm@PATTERJM-W540.rose-hulman.edu>
2016-09-28 03:40:39 -0400

    GUI minisprint update
    add datasheet tab 
    remove some debugging code
    remove unnecessary code

--- src/main/java/com/metsci/laproc/App.java
+++ src/main/java/com/metsci/laproc/App.java
@@ -37,61 +37,12 @@ public class App {
 
         JTabbedPane tabbedPane = new JTabbedPane();
         frame.add(tabbedPane);
-       /* NewtSwingGlimpseCanvas canvas = new NewtSwingGlimpseCanvas( );
+        NewtSwingGlimpseCanvas canvas = new NewtSwingGlimpseCanvas( );
         canvas.addLayout(new ROCCurvePlot(graph).getLayout());
         new FPSAnimator( canvas.getGLDrawable( ), 120 ).start( );
-        tabbedPane.add("ROCCurvePlot",canvas);*/
-        
-        JPanel borderPanel = new JPanel();
-        GridBagLayout ogbl = new GridBagLayout();
-        borderPanel.setLayout(ogbl);
-        JPanel dataPanel = new JPanel();
-        GridBagLayout gbl = new GridBagLayout();
-        gbl.maximumLayoutSize(frame);
-        
-        dataPanel.setLayout(gbl);
-        for(int i = 0; i < 5; i++){
-        	JLabel tempLabel = new JLabel("Header " + Integer.toString(i));
-        	tempLabel.setBorder(BorderFactory.createCompoundBorder(
-        			BorderFactory.createLineBorder(Color.BLACK,1,false), 
-        			BorderFactory.createEmptyBorder(10, 10, 10, 10)));
-        	GridBagConstraints c = new GridBagConstraints();
-        	c.fill = GridBagConstraints.HORIZONTAL;
-        	c.gridx = i;
-        	c.gridy = 0;
-        	dataPanel.add(tempLabel, c);
-        }
-        for(int i = 1; i < 7; i++){
-            for(int j = 0; j < 5; j++){
-            	JLabel tempLabel = new JLabel("Entry " + Integer.toString(i) + " value " + Integer.toString(j));
-            	tempLabel.setBorder(BorderFactory.createCompoundBorder(
-            			BorderFactory.createLineBorder(Color.BLACK,1,false), 
-            			BorderFactory.createEmptyBorder(10, 10, 10, 10)));
-            	GridBagConstraints c = new GridBagConstraints();
-            	c.fill = GridBagConstraints.HORIZONTAL;
-            	c.gridx = j;
-            	c.gridy = i;
-            	dataPanel.add(tempLabel, c);
-            }
-        }
-        GridBagConstraints c = new GridBagConstraints();
-    	//c.fill = GridBagConstraints.HORIZONTAL;
-    	c.gridx = 5;
-    	c.gridy = 0;
-    	c.gridwidth = 5;
-        dataPanel.setBorder(BorderFactory.createLineBorder(Color.RED,2,false));
-        borderPanel.add(dataPanel, c);
-        GridBagConstraints c2 = new GridBagConstraints();
-    	//c2.fill = GridBagConstraints.HORIZONTAL;
-    	c2.gridx = 0;
-    	c2.gridy = 0;
-    	c2.gridwidth = 5;
-    	JPanel leftPanel = new JPanel();
-    	leftPanel.setLayout(new BorderLayout());
-    	leftPanel.add(new JLabel("Data Sets"), BorderLayout.NORTH);
-    	leftPanel.add(new JLabel("center test"), BorderLayout.CENTER);
-    	borderPanel.add(leftPanel, c2);
-        borderPanel.setBorder(BorderFactory.createLineBorder(Color.BLUE,2,false));
-        tabbedPane.addTab("Datasheet", borderPanel);
+        tabbedPane.add("ROCCurvePlot",canvas);
+       
+        JPanel dataPanel = DataSheetGUI.GetDataSheet(); 
+        tabbedPane.addTab("Datasheet", dataPanel);
     }
 }
--- src/main/java/com/metsci/laproc/DataSheetGUI.java
+++ src/main/java/com/metsci/laproc/DataSheetGUI.java
@@ -1,13 +1,48 @@
 package com.metsci.laproc;
 
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class DataSheetGUI extends GridBagLayout{
+
+public class DataSheetGUI{
 	
-	public DataSheetGUI(){
-		super();
+	public static JPanel GetDataSheet(){
+        JPanel dataPanel = new JPanel();
+        GridBagLayout gbl = new GridBagLayout();
+        dataPanel.setLayout(gbl);
+        JLabel tempLabel;
+        GridBagConstraints c;
+        for(int i = 0; i < 5; i++){
+        	tempLabel = new JLabel("Header " + Integer.toString(i));
+        	tempLabel.setBorder(BorderFactory.createCompoundBorder(
+        			BorderFactory.createLineBorder(Color.BLACK,1,false), 
+        			BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+        	c = new GridBagConstraints();
+        	c.fill = GridBagConstraints.HORIZONTAL;
+        	c.gridx = i;
+        	c.gridy = 0;
+        	dataPanel.add(tempLabel, c);
+        }
+        for(int i = 1; i < 7; i++){
+            for(int j = 0; j < 5; j++){
+            	tempLabel = new JLabel("Entry " + Integer.toString(i) + " value " + Integer.toString(j));
+            	tempLabel.setBorder(BorderFactory.createCompoundBorder(
+            			BorderFactory.createLineBorder(Color.BLACK,1,false), 
+            			BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+            	c = new GridBagConstraints();
+            	c.fill = GridBagConstraints.HORIZONTAL;
+            	c.gridx = j;
+            	c.gridy = i;
+            	dataPanel.add(tempLabel, c);
+            }
+        }
+        return dataPanel;
 	}
 	
 }
