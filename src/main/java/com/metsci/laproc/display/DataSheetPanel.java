f341ccb59d97009c00e87485627b34322cba428f
Jacob Patterson <Jacob Patterson>
2016-10-14 00:38:39 -0400

    add comments in preperation for merge

--- src/main/java/com/metsci/laproc/display/DataSheetPanel.java
+++ src/main/java/com/metsci/laproc/display/DataSheetPanel.java
@@ -13,15 +13,28 @@ import javax.swing.table.TableColumn;
 
 import com.metsci.laproc.data.ClassifierDataSet;
 import com.metsci.laproc.data.DataPoint;
-
+/**
+ * 
+ * A datasheet panel creator.
+ * Created by patterjm on 10/5/2016.
+ *
+ */
 public class DataSheetPanel {
-
+	/**
+	 * Return DataSheet, composed of JTable
+	 * 
+	 * @params: ClassifierDataSet
+	 */
 	public static JScrollPane GetDataSheet(ClassifierDataSet data) {
-		int sizeOfArray = data.getAllPoints().next().getValues().length + 1;
+		//instantiate variables
+		int sizeOfArray = data.getAllPoints().next().getValues().length + 1;//+1 for truth column
 		String[] columnNames = new String[sizeOfArray];
 		Object[][] dataList = new Object[100][100];
 		int tempCounter = 0;
+		
+		//write in all data into a 2D Object array, JTable requirement
 		for(DataPoint dataPoint : data){
+			//when at max length, double array size and copy contents over
 			if(tempCounter == dataList.length){
 				Object[][] newArray = new Object[dataList.length*2][sizeOfArray];
 				for(int i = 0; i < dataList.length; i++){
@@ -31,18 +44,23 @@ public class DataSheetPanel {
 				}
 				dataList = newArray;
 			}
+			
 			Object[] datavals = new Object[sizeOfArray];
 			double[] getValTemp = dataPoint.getValues();
+			
 			for(int i = 1; i < sizeOfArray; i++){
 				datavals[i] = getValTemp[i-1];
 			}
 			datavals[0] = dataPoint.getTruth();
 			dataList[tempCounter++] = datavals;
 		}
+		
 		//TODO: Implement column headers
 		for(int i = 0; i < sizeOfArray; i++){
 			columnNames[i] = "Woohoo";
 		}
+		
+		//Init Java Swing settings
 		JTable table = new JTable(dataList, columnNames);
 		JScrollPane scrollPane = new JScrollPane(table);
 		table.setFillsViewportHeight(true);
