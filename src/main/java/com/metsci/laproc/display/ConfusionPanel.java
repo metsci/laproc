9796826eeebc8a4b48d6fbee02e3b2534fcbc61b
Jack Porter <porterjc@rose-hulman.edu>
2016-10-18 16:39:00 -0400

    added basic shell for a confusion matrix panel

---
+++ src/main/java/com/metsci/laproc/display/ConfusionPanel.java
@@ -0,0 +1,16 @@
+package com.metsci.laproc.display;
+
+import javax.swing.*;
+
+/**
+ *
+ *
+ * Created by porterjc on 10/14/2016.
+ */
+public class ConfusionPanel {
+
+    public JPanel getConfusionMatrix(){
+
+        return null;
+    }
+}
