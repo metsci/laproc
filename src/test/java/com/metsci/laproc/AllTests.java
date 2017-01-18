5530c941543835cf6220204b5351f0c164bcc583
chris <christopher.malinosky@gmail.com>
2017-01-18 16:26:17 -0500

    Added testing suite and test for observable

---
+++ src/test/java/com/metsci/laproc/AllTests.java
@@ -0,0 +1,22 @@
+package com.metsci.laproc;
+
+import com.metsci.laproc.plotting.GraphTest;
+import com.metsci.laproc.plotting.ROCCurveTest;
+import com.metsci.laproc.pointmetrics.MetricsTest;
+import com.metsci.laproc.utils.ObservableTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by malinocr on 1/18/2017.
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ GraphTest.class,
+        ROCCurveTest.class,
+        MetricsTest.class,
+        AppTest.class,
+        ObservableTest.class})
+public class AllTests {
+
+}
